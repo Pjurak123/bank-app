16bb44d33966d4d55e55ff2178e595d15b1315e4
pjurak <pjurak3@gmail.com>
2022-04-11 16:36:59 +0200

    Deleted stuff

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,5 @@
 import React from "react";
 import "../Footer/Footer.css";
-import facebook from "../../images/icon-facebook.svg";
-import youtube from "../../images/icon-youtube.svg";
-import twitter from "../../images/icon-twitter.svg";
-import pinterest from "../../images/icon-pinterest.svg";
-import instagram from "../../images/icon-instagram.svg";
 import { Logo } from "./Logo";
 import { SocialIcons } from "./SocialIcons";
 
